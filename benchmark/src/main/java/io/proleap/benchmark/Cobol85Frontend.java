353a2394027778ec0a630ad34cb64667814aec7f
Gustavo Oliveira <gustavo28064212@gmail.com>
2026-08-28 11:31:09 -0300

    remove ruido

--- benchmark/src/main/java/io/proleap/benchmark/Cobol85Frontend.java
+++
@@ -1,8 +0,0 @@
-package io.proleap.benchmark;
-
-import java.io.IOException;
-import java.nio.file.Path;
-
-public final class Cobol85Frontend extends AntlrCobolFrontend {
-    public Cobol85Frontend(Path copybooks) throws IOException { super(Bindings.cobol85(), copybooks); }
-}
--- benchmark/src/main/java/io/proleap/benchmark/CobolFrontend.java
+++
@@ -1,8 +0,0 @@
-package io.proleap.benchmark;
-
-import java.nio.file.Path;
-
-public interface CobolFrontend {
-    String name();
-    FrontendResult parse(Path source);
-}
